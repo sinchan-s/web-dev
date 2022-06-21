5782176500d17482732de967e8dac09cb2de015c
Sinchan <sinchan.tex@gmail.com>
2022-06-21 23:31:40 +0530

    working tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  content: ['./dist/*.html'],
+  theme: { 
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors:{
+        brightRed: 'hsl(12, 88%, 59%)',
+      }
+    },
   },
   plugins: [],
 }
